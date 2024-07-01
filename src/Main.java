34976615153838c012ea7c9a077eae90ee4374ae
onlyplsson <onlyplsson@gmail.com>
2024-07-01 17:47:44 +0900

    :memo: 문제 풀이 파일명 수정

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-
+        String[] arr = {"a", "b", "c"};
+        List<String> list = Arrays.asList(arr);
+        String tmp = list.get(1);
+        list.set(1, list.get(0));
+        list.set(0, tmp);
+        System.out.println(list.toString());
     }
 }
