046483240d846a41376dde9b39693e50a9394150
Junggyun Park <92567159+junggyun@users.noreply.github.com>
2024-10-16 18:44:31 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 77.43 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,24 +4,21 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         
-        HashMap<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < completion.length; i++) {
-            if (map.containsKey(completion[i])) {
-                map.put(completion[i], map.get(completion[i]) + 1);
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (String player : completion) {
+            if (map.containsKey(player)) {
+                map.put(player, map.get(player) + 1);
             } else {
-                map.put(completion[i], 1);
+                map.put(player, 1);
             }
         }
         
-        for (int i = 0; i < participant.length; i++) {
-            if (map.containsKey(participant[i])) {
-                if (map.get(participant[i]).equals(1)) {
-                    map.remove(participant[i]);
-                } else {
-                    map.put(participant[i], map.get(participant[i]) - 1);
-                }
+        for (String player : participant) {
+            if (!map.containsKey(player) || map.get(player).equals(0)) {
+                answer = player;
             } else {
-                answer = participant[i];
+                map.put(player, map.get(player) - 1);
             }
         }
         
