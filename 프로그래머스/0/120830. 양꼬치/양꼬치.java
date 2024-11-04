aade901aceab5eaca5b043e4419ae6ea6f4574f0
Junggyun Park <92567159+junggyun@users.noreply.github.com>
2024-11-04 12:29:14 +0900

    [level 0] Title: 양꼬치, Time: 0.04 ms, Memory: 85.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        k -= n / 10;
+        answer += n * 12000;
+        answer += k * 2000;
+        
+        return answer;
+    }
+}
