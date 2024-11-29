787738c3ab837cf7b04136da5a8c6fca6475f1e1
Junggyun Park <92567159+junggyun@users.noreply.github.com>
2024-11-29 15:57:39 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 85.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        if (hp / 5 > 0) {
+            answer += hp / 5;
+            hp = hp % 5;
+        }
+        if (hp / 3 > 0) {
+            answer += hp / 3;
+            hp = hp % 3;
+        }
+        if (hp / 1 > 0) {
+            answer += hp / 1;
+            hp = hp % 1;
+        }
+        
+        return answer;
+    }
+}
