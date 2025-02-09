c99d0f27104ec23a832827f811ebdf7df5ab98d7
Junggyun Park <92567159+junggyun@users.noreply.github.com>
2025-02-09 21:07:49 +0900

    [level 0] Title: 수 조작하기 2, Time: 11.63 ms, Memory: 94.5 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,24 +1,21 @@
+import java.util.*;
+
 class Solution {
     public String solution(int[] numLog) {
         StringBuilder sb = new StringBuilder();
+        
+        Map<Integer, String> control = new HashMap<>();
+        control.put(1, "w");
+        control.put(10, "d");
+        control.put(-1, "s");
+        control.put(-10, "a");
+        
         int change = 0;
         for (int i = 0; i < numLog.length - 1; i++ ) {
             change = numLog[i + 1] - numLog[i];
-            switch (change) {
-                case 1:
-                    sb.append("w");
-                    break;
-                case -1:
-                    sb.append("s");
-                    break;
-                case 10:
-                    sb.append("d");
-                    break;
-                case -10:
-                    sb.append("a");
-                    break;
-            }
+            sb.append(control.get(change));
         }
+        
         return sb.toString();
     }
 }
