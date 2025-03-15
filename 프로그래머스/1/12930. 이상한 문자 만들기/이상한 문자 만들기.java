0012ccf14a4ecc69d52a617d10a75dc05f3a56b7
Junggyun Park <92567159+junggyun@users.noreply.github.com>
2025-03-16 01:18:14 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.30 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -5,13 +5,8 @@ class Solution {
         int index = 0;
         for (int i = 0; i < s.length(); i++) {
             String target = s.substring(i, i + 1);
-            if (target.equals(" ")) {
-                index = 0;
-                sb.append(" ");
-            } else {
-                sb.append(index % 2 == 0 ? target.toUpperCase() : target.toLowerCase());
-                index++;
-            }
+            sb.append(index % 2 == 0 ? target.toUpperCase() : target.toLowerCase());
+            index = target.equals(" ") ? 0 : index + 1;
         }
         
         return sb.toString();
