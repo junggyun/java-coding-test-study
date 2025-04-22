6e8ab2d863d235f3beeeeb785c84beb8b0e28f61
Junggyun Park <92567159+junggyun@users.noreply.github.com>
2025-04-22 16:52:50 +0900

    [level 3] Title: 네트워크, Time: 0.21 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,27 +1,25 @@
 class Solution {
-    private static int[][] graph;
-    private static boolean[] visited;
-    
+    static boolean[] visited;
+    static int[][] graph;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        graph = computers;
         visited = new boolean[n];
-        
+        graph = computers;
         for (int i = 0; i < n; i++) {
-            if (visited[i] || computers[i][i] == 0) continue;
-            dfs(i);
-            answer++;
+            if (!visited[i]) {
+                dfs(i);
+                answer++;;
+            }
         }
         return answer;
     }
     
-    private static void dfs(int x) {
-        visited[x] = true;
-        for (int i = 0; i < graph[x].length; i++) {
-            int y = graph[x][i];
-            if (y == 0) continue;
-            if (!visited[i]) dfs(i);
+    static void dfs(int target) {
+        visited[target] = true;
+        for (int i = 0; i < graph.length; i++) {
+            if (!visited[i] && graph[target][i] == 1) {
+                dfs(i);
+            }
         }
     }
-    
 }
