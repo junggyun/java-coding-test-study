f0609bafa943e7450a7da73fbc43a50ae0c2cfba
Junggyun Park <92567159+junggyun@users.noreply.github.com>
2025-06-07 16:07:40 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.16 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[][] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr[i].length; j++) {
+                if (arr[i][j] != arr[j][i]) {
+                    return 0;
+                }
+            }
+        }
+        return 1;
+    }
+}
